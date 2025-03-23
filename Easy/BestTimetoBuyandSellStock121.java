592938e68e7a04003c222789dfdaf4c4b2f1493c
kkevinramirez <kkevramirez@gmail.com>
2025-03-22 22:59:27 -0400

    121

---
+++ Easy/BestTimetoBuyandSellStock121.java
@@ -0,0 +1,15 @@
+public class BestTimetoBuyandSellStock121 {
+    public int maxProfit(int [] prices) {
+        int min_val = Integer.MAX_VALUE;
+        int max_profit = 0;
+
+        for(int i = 0; i<prices.length; i++) {
+            if (prices[i] < min_val) {
+                min_val = prices[i];
+            } else if (prices[i] - min_val > min_val) {
+                max_profit = prices[i] - min_val;
+            }
+        }
+        return max_profit;
+    }
+}
