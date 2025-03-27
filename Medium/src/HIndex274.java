6e4801bc4bbb05c693147c76b2f069cc7dc6b009
kkevinramirez <kkevramirez@gmail.com>
2025-03-26 22:30:10 -0400

    274

--- Medium/src/HIndex274.java
+++ Medium/src/HIndex274.java
@@ -1,12 +1,20 @@
+import java.util.*;
+
+
 public class HIndex274 {
     public int hIndex(int [] citations) {
-        
+        Arrays.sort(citations);
+        int n = citations.length;
+
+        for (int i = 0; i < citations.length; i++) {
+            int h = n - i;
+
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+        return 0;
     }
 }
 
 
-
-n == citations.length
-1 <= n <= 5000
-
-        0 <= citations[i] <= 1000
